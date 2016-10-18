610bb16371d230dbc36a1be3012e750d32016fd0
Michelle Lawrence <michelle.lawrence@utexas.edu>
2016-10-18 12:51:31 -0500

    changed api calls to https for github security

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ var getUnanswered = function(tags) {
 	};
 	
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		url: "https://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET",
@@ -121,7 +121,7 @@ var getTopAnswered = function(tags) {
 		sort: 'creation'
 	};
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/tags/" + tags + "/top-answerers/all_time",
+		url: "https://api.stackexchange.com/2.2/tags/" + tags + "/top-answerers/all_time",
 		data: request,
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET",
